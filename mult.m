1e17c06ba74fd0b6acb9c4beb5f20904f6e48477
margotduek <margotduek@gmail.com>
2015-01-23 12:54:15 -0600

    :bow: programa de potencia

---
+++ mult.m
@@ -0,0 +1,11 @@
+%Margot Duek
+%Metodos numericos para la ingenieria
+%23/01/14
+%Programa que saca la potencia de dos numeros
+
+function res = mult(a ,b )
+  res = 1;
+  for i = 1:b
+    res *= a ;
+  end
+end
